b773e11692feb01413e7b97dd283682719e1faff
LNFWebsite <LNFWebsite@users.noreply.github.com>
2015-08-28 12:49:48 -0400

    going to set up pause with spacebar

--- index.js
+++ index.js
@@ -104,6 +104,7 @@ function pauseVideo() {
     stayPaused = false;
   }
   $("#pauseOverlay").css("display", "none");
+  $("youtube").focus();
   setTimeout(function(){
     $("#pauseOverlay").css("display", "block");
     $("#youtube").blur();
