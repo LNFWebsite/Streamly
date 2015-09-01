a9ed258cc009ee7c79513abfec04e9651e0a026d
LNFWebsite <LNFWebsite@users.noreply.github.com>
2015-09-01 19:08:11 -0400

    alright lets test this out

--- index.js
+++ index.js
@@ -94,13 +94,14 @@ function playVideo() {
   }, 3000);
   
   var startTime = new Date();
-  /***
-  function progressLoop()
-  window.setTimeout(function() {
-    var currentTime = new Date() - startTime;
-    console.log("time elapsed: " + currentTime);
-  }, 1000);
-  ***/
+  
+  function progressLoop() {
+    window.setTimeout(function() {
+      var currentTime = new Date() - startTime;
+      console.log("time elapsed: " + currentTime);
+      progressLoop();
+    }, 1000);
+  }
 }
 
 function loopVideo() {
