857fec04cb6cc1be695cb457d375ed2432e3745c
LNFWebsite <LNFWebsite@users.noreply.github.com>
2015-10-01 14:32:37 -0400

    ...

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ function loopVideo() {
     else {
       actionTimers.clear();
       $("#youtube").attr("src", "");
-      if (videos[0] !== undefined) {
+      if (videos[0] !== undefined && videos[0] !== null) {
         document.title = "Streamly - " + decodeURIComponent(videos[0]);
       }
       else {
@@ -165,7 +165,7 @@ function pauseVideo() {
   if (!videoPaused) {
     actionTimers.pause();
     videoPaused = true;
-    if (videos[0] !== undefined) {
+    if (videos[0] !== undefined && videos[0] !== null) {
       document.title = "Streamly - " + decodeURIComponent(videos[0]);
     }
   }
@@ -231,7 +231,7 @@ function getPlaylist() {
     playlist = JSON.parse(playlist);
     videos = playlist;
     
-    if (videos[0] !== undefined) {
+    if (videos[0] !== undefined && videos[0] !== null) {
       $("#playlistNameBox").val(decodeURIComponent(videos[0]));
     }
     
