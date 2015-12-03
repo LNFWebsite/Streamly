e65be5e8c5326153970e8db22014f3dc13ad0967
LNFWebsite <LNFWebsite@users.noreply.github.com>
2015-12-02 20:52:32 -0500

    ...

--- index.js
+++ index.js
@@ -388,7 +388,7 @@ function input(type) {
   switch (type) {
     case 0:
       if (inputBox != "") {
-        popup = window.open("https://www.youtube.com/results?search_query=" + inputBox.replace(/ /g, "+"), "YouTube", "height=500,width=800,chrome");
+        popup = window.open("https://www.youtube.com/results?search_query=" + inputBox.replace(/ /g, "+"), "YouTube", "height=500,width=800,chrome=yes");
         $("#inputBox").val("").attr("placeholder", placeholder);
       }
       break;
