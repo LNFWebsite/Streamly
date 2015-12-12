d0b8e1a309e06e9115efc9d71e67e0216ad235ba
LNFWebsite <LNFWebsite@users.noreply.github.com>
2015-12-11 21:13:11 -0500

    just less than 0... because if it is 0 then the video to be played is 1

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function changeIteration(which) {
   if (playlistRepeat && sum > videoCounter) {
     return 1;
   }
-  else if (playlistRepeat && sum <= 0) {
+  else if (playlistRepeat && sum < 0) {
     return videoCounter - 1;
   }
   else {
