c2bb2a291390e05ac8bcc4e41bfd19551281680c
LNFWebsite <LNFWebsite@users.noreply.github.com>
2016-04-14 11:49:08 -0400

    add global flag

--- index.js
+++ index.js
@@ -331,7 +331,7 @@ function setAutoplay() {
       success: function(res) {
         var data = res["responseText"];
         try {
-          var regex = /<li class=\"yt-uix-scroller-scroll-unit(?:.|\n)*?data-video-id=\"(.+?)\"/i;
+          var regex = /<li class=\"yt-uix-scroller-scroll-unit(?:.|\n)*?data-video-id=\"(.+?)\"/ig;
           var data = data.match(regex);
           console.log(data);
           videoUrl = "https://www.youtube.com/watch?v=" + data[1][1];
