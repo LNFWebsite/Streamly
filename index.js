799b96c4a47f7e12d4f757b5fb73b5526ab21ae2
LNFWebsite <LNFWebsite@users.noreply.github.com>
2016-09-14 13:16:51 -0400

    check if popup closed... loop required http://stackoverflow.com/a/5712425

--- index.js
+++ index.js
@@ -613,6 +613,15 @@ function input(type) {
         }
         else {
           popup = window.open("https://www.youtube.com/results?search_query=" + inputBox.replace(/ /g, "+"), "YouTube", "height=500,width=800");
+          
+          var checkIfClosedTimer = setInterval(checkIfClosed(), 500);
+          function checkIfClosed() {
+              if (popup.closed) {
+                $("#youtube").css("display", "block");
+                clearInterval(timer);
+              }
+          }
+          
           $("#inputBox").val("").attr("placeholder", placeholder);
           $("#youtube").css("display", "none");
         }
