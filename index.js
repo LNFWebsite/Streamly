df4503e64ddd1526dc61fd8f817e3dfe58c83dcb
LNFWebsite <LNFWebsite@users.noreply.github.com>
2016-09-14 18:46:42 -0400

    MORE DEBUGGING

--- index.js
+++ index.js
@@ -114,6 +114,7 @@ var ActionTimers = function() {
 var actionTimers = new ActionTimers();
 
 function playVideo() {
+  console.log("playVideo!");
   highlight(videoIteration, "selected");
   addAutoplayVideo();
   videoPreviews();
@@ -137,6 +138,7 @@ function playVideo() {
 }
 
 function loopVideo() {
+  console.log("loopVideo!");
   videoIteration = changeIteration(1);
   playVideo();
 
@@ -178,6 +180,7 @@ var VideoFunctions = function() {
   this.loop = function() {
     if (videoIteration < videoCounter || playlistRepeat) {
       actionTimers.clear();
+      console.log("loop message!");
       loopVideo();
     }
     else {
