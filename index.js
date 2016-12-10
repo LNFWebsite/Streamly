5bf3cd3362cd0669b53839134918705bffc838fc
LNFWebsite <LNFWebsite@users.noreply.github.com>
2016-12-09 21:38:26 -0500

    let's just get this over with

--- index.js
+++ index.js
@@ -420,8 +420,7 @@ function shuffleArray(array) {
 function shufflePlaylist() {
   console.log("playlistShuffle: " + playlistShuffle);
   if (playlistShuffle) {
-    videosBeforeShuffle = videos.splice(0);
-    videos = videosBeforeShuffle;
+    videosBeforeShuffle = [videos];
     var playlistName = videos[0];
     var videoIterationId = videos[videoIteration][2];
     videos.splice(0, 1);
@@ -429,7 +428,7 @@ function shufflePlaylist() {
     videos.unshift(playlistName);
   }
   else {
-    videos = videosBeforeShuffle;
+    videos = videosBeforeShuffle[0];
   }
   
   for (var i = 1; i < videos.length; i++) {
