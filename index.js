e280bf95c0e249d633dd4e0e93815a64b0be3b68
LNFWebsite <LNFWebsite@users.noreply.github.com>
2017-03-10 12:08:15 -0500

    see if it's because it

--- index.js
+++ index.js
@@ -384,13 +384,13 @@ function quickSearch(query) {
 }
 
 function onSearchDataPlayerReady(query) {
-  var setAVar = "Blahblah";
-  searchDataPlayer.cuePlaylist({listType: "search", list: query});
+  searchDataPlayer.cuePlaylist({list:"RDJn-k66pQ0H4"});
 }
 
 function onSearchDataPlayerStateChange(event) {
   console.log("does it ever get here: " + event.data);
   if (event.data === 5) {
+    /**
     $("#inputBox").val("").attr("placeholder", placeholder).blur().focus();
     quickSearchVideosIteration = 0;
     quickSearchVideos = searchDataPlayer.getPlaylist();
@@ -401,6 +401,7 @@ function onSearchDataPlayerStateChange(event) {
     var videoTime = Math.round(searchDataPlayer.getDuration());
     //searchDataPlayer.destroy();
     addVideo(videoName, videoTime, videoId);
+    **/
   }
 }
 
