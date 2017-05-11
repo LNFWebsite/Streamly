a75068b4c53124904e92fdbd6057de3526eab0de
LNFWebsite <LNFWebsite@users.noreply.github.com>
2017-05-11 14:54:15 -0400

    oops...

--- index.js
+++ index.js
@@ -833,7 +833,7 @@ function stationLoaded() {
 
 function station(server) {
   $.ajax({
-    url: "http://" + server + "socket.io/socket.io.js",
+    url: "http://" + server + "/socket.io/socket.io.js",
     dataType: "script",
     success: stationLoaded
   });
