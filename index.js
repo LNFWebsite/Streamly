968e7104521e7562aef0b836f1a737fa311dc6d5
LNFWebsite <LNFWebsite@users.noreply.github.com>
2017-05-13 11:05:45 -0400

    NAN error

--- index.js
+++ index.js
@@ -912,7 +912,8 @@ function loadStation() {
         case "actionmovevideo":
           actionMoveVideo(+msgData[2], +msgData[3]);
           var from = "#videosTable tr:nth-child(" + msgData[2] + ")";
-          var to = "#videosTable tr:nth-child(" + (+msgData[3] - 1) + ")";
+          var toIteration = msgData[3] - 1;
+          var to = "#videosTable tr:nth-child(" + toIteration + ")";
           console.log("FROM: " + from);
           console.log("TO: " + to);
           $(to).after($(from));
