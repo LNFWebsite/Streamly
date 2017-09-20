6f7b1b321d0f8bd23a201f1549d3188fdcf14229
LNFWebsite <LNFWebsite@users.noreply.github.com>
2017-09-20 17:31:40 -0400

    debugging the bug where backRestart is finnicky

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ var videoPaused;
 //this var is for addVideo knowing whether to loop to next video or not
 var videoPlaying;
 var backRestart;
+var backRestartTimer;
 
 var progressTimer;
 
@@ -224,12 +225,11 @@ function playVideo() {
     }
     console.log("Debug: playVideo");
   }
-  if (!videoPaused) {
-    backRestart = false;
-    window.setTimeout(function() {
-      backRestart = true;
-    }, 3000);
-  }
+  backRestart = false;
+  clearTimeout(backRestartTimer);
+  backRestartTimer = window.setTimeout(function() {
+    backRestart = true;
+  }, 3000);
 }
 
 function loopVideo() {
