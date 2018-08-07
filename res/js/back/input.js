0544d25d74540a271fa26c4b6fd3ac4cb443e182
LNFWebsite <LNFWebsite@users.noreply.github.com>
2018-08-07 13:36:11 -0400

    this won't have any effect

--- res/js/back/input.js
+++ res/js/back/input.js
@@ -61,7 +61,6 @@ function input(type) {
       videos[0] = undefined;
     }
     setPlaylist();
-    $("#ogTitle").attr("content", "Streamly - " + playlistNameBox);
   }
   else {
     if (inputBox !== "") {
