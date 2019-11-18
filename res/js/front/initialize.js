137534a0639e8fdc4a0beb6f65c18b64cb5de94d
LNFWebsite <LNFWebsite@users.noreply.github.com>
2019-11-18 15:30:59 -0500

    chrome useragent workaround

--- res/js/front/initialize.js
+++ res/js/front/initialize.js
@@ -51,4 +51,9 @@ window.onload = function() {
   if (cookie.get("sbs")) {
     toggleSBS();
   }
+  
+  //set userAgent for mobile Chrome workaround
+  Object.defineProperty(navigator, 'userAgent', {
+    get: function () { return 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36'; }
+  });
 }
