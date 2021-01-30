843a6741460440b310a8129e0f53c1269a512909
LNFWebsite <LNFWebsite@users.noreply.github.com>
2021-01-29 19:28:10 -0500

    Update toggles.js

--- res/js/back/toggles.js
+++ res/js/back/toggles.js
@@ -84,12 +84,10 @@ function toggleMenu(which) {
 
 let DropOverlay = function() {
   this.open = function() {
-    $("#dropShadow").css("display", "block");
-    $("#dropOverlay").css("display", "block");
+    $("#dropShadow, #dropOverlay").css("display", "initial");
   }
   this.close = function() {
-    $("#dropShadow").css("display", "none");
-    $("#dropOverlay").css("display", "none");
+    $("#dropShadow, #dropOverlay").css("display", "none");
   }
 }
 let dropOverlay = new DropOverlay();
